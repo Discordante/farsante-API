9a3f002d621df80fb55bc99267a99c1f9faa11db
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-05-20 13:32:35 +0200

    Add basic route and controller

---
+++ src/controllers/people.controller.js
@@ -0,0 +1,14 @@
+const createError = require('http-errors')
+
+const names = require('../bin/names.json')
+
+module.exports.getRandomName = (req, res, next) => {
+
+
+
+  
+  res.json(names)
+  //next(createError(404, { errors: { email: 'Email or password is incorrect' }}))
+
+    
+}
