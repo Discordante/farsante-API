0cecb7ff1b8883715f959be23d232790f55ea1f3
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-05-22 13:42:37 +0200

    Add key in dni generator response

--- src/controllers/document.controller.js
+++ src/controllers/document.controller.js
@@ -5,7 +5,10 @@ const {dniGenerator} = require('../functions/functions')
 
 
 module.exports.getDNI = (req, res, next) => {
-    res.status(200).json(dniGenerator())
+    let dniNumber= {
+        dni: dniGenerator()
+    }
+    res.status(200).json(dniNumber)
 }
 
 
