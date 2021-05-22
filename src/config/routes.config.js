8aad557b25872a85957ec6f0ac89e7594d5eb06e
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-05-22 11:13:01 +0200

    Add document router

--- src/config/routes.config.js
+++ src/config/routes.config.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const peopleController = require('../controllers/people.controller')
+const documentController = require('../controllers/document.controller')
 
 router.get('/randomName', peopleController.getRandomName)
 router.get('/femaleName', peopleController.getFemaleName)
 router.get('/maleName', peopleController.getMaleName)
 router.get('/lastName', peopleController.getLastName)
+
+router.get('/dni', documentController.getDNI)
 
 module.exports = router;
