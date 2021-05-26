36c3bcef7df037cdf74c4c868f4ce696719a4abb
Discordante <diegocerratos@gmail.com>
2021-05-26 11:47:37 +0200

    Add phone number route

--- src/config/routes.config.js
+++ src/config/routes.config.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const peopleController = require('../controllers/people.controller')
 const documentController = require('../controllers/document.controller')
 const placesController = require('../controllers/places.controller')
+const placesContact = require('../controllers/contact.controller')
 
 //people
 router.get('/randomName', peopleController.getRandomName)
 router.get('/femaleName', peopleController.getFemaleName)
 router.get('/maleName', peopleController.getMaleName)
 router.get('/lastName', peopleController.getLastName)
+
+//contact
+router.get('/phoneNumber', placesContact.getPhoneNumber)
 
 //documentation
 router.get('/dni', documentController.getDNI)
