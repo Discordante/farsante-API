d9751f3f21f5c590341b99e4303248b22f3e23ca
Discordante <diegocerratos@gmail.com>
2021-05-27 13:11:21 +0200

    Add getEmail controller and email domain constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,3 @@
 module.exports.NIF_LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+module.exports.EMAIL_DOMAIN = ["gmail","yahoo","hotmail","mail", "outlook", "mandrill", "mailgun", "msn", "freshmail", "testmail"];
